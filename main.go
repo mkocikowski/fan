bc1326364ab217bae10972870689f1cc51d99d75
Mik Kocikowski <mkocikowski@gmail.com>
2016-03-24 16:37:08 -0700

    Cleaned up vars names and commented.

--- main.go
+++ main.go
@@ -10,44 +10,52 @@ import (
 	"sync"
 )
 
-func run(in, out chan []byte, wg *sync.WaitGroup) {
-
-	wg.Add(1)
-	defer wg.Done()
-
-	cmd := exec.Command(flag.Args()[0], flag.Args()[1:]...)
-	stdin, _ := cmd.StdinPipe()
-	stdout, _ := cmd.StdoutPipe()
-	cmd.Start()
-
-	scanner := bufio.NewScanner(stdout)
-	go func() {
-		wg.Add(1)
-		defer wg.Done()
-		for scanner.Scan() {
-			b := scanner.Bytes()
-			c := make([]byte, len(b), len(b)+1)
-			copy(c, b)
-			out <- append(c, '\n')
-		}
-	}()
-
-	for b := range in {
-		i, err := stdin.Write(b)
-		if err != nil {
-			log.Println(i, err)
-		}
-	}
-
-	stdin.Close() // signal for child process to exit
-	cmd.Wait()
-}
+var (
+	inChan  = make(chan []byte)
+	outChan = make(chan []byte)
+	wg      sync.WaitGroup
+)
 
 func init() {
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "usage: np [-n numprocs] command\n")
 		flag.PrintDefaults()
 	}
+}
+
+func worker() {
+	defer wg.Done()
+
+	cmd := exec.Command(flag.Args()[0], flag.Args()[1:]...)
+	cmdStdin, err := cmd.StdinPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	cmdStdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	cmd.Start()
+
+	output := bufio.NewScanner(cmdStdout)
+	go func() {
+		for output.Scan() {
+			b := output.Bytes()
+			c := make([]byte, len(b), len(b)+1)
+			copy(c, b)
+			outChan <- append(c, '\n')
+		}
+	}()
+
+	for line := range inChan {
+		i, err := cmdStdin.Write(line)
+		if err != nil {
+			log.Println(i, err)
+		}
+	}
+
+	cmdStdin.Close() // signal for child process to exit
+	cmd.Wait()
 }
 
 func main() {
@@ -60,28 +68,26 @@ func main() {
 		os.Exit(1)
 	}
 
-	in := make(chan []byte)
-	out := make(chan []byte)
-
-	var wg sync.WaitGroup
 	for i := 0; i < n; i++ {
-		go run(in, out, &wg)
+		wg.Add(1)
+		go worker()
 	}
 
 	go func() {
-		scanner := bufio.NewScanner(os.Stdin)
-		for scanner.Scan() {
-			b := scanner.Bytes()
+		input := bufio.NewScanner(os.Stdin)
+		for input.Scan() {
+			b := input.Bytes()
 			c := make([]byte, len(b), len(b)+1)
 			copy(c, b)
-			in <- append(c, '\n')
+			inChan <- append(c, '\n')
 		}
-		close(in)
-		wg.Wait() // wait for all children to finish
-		close(out)
+		close(inChan)  // signal to workers to exit
+		wg.Wait()      // wait for all workers
+		close(outChan) // signal completion to exit main loop
 	}()
 
-	for b := range out {
-		os.Stdout.Write(b)
+	// this when exit when outChan closed by the goroutine above
+	for line := range outChan {
+		os.Stdout.Write(line)
 	}
 }
